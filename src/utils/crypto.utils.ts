e13759dfbcadccce07c7beb24a265f56bc44caa9
Vinit Kumar <vinit928@gmail.com>
2024-02-29 14:02:52 +0530

    added crypto

--- src/utils/crypto.utils.ts
+++ src/utils/crypto.utils.ts
@@ -1,3 +1,5 @@
+import * as crypto from "node:crypto";
+
 export function encrypt(password: string): string {
-  return password;
+  return crypto.createHash("sha256").update(password).digest("hex");
 }
